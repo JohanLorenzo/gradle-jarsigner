4be3b83ae23008ed0939954a1960b82cd8998002
Johan Lorenzo <jlorenzo@mozilla.com>
2018-05-21 15:47:54 +0200

    Make keystore population independent from the number of dependencies

--- src/main/groovy/org/mozilla/gradle_jarsigner/JarSignerVerifyPlugin.groovy
+++ src/main/groovy/org/mozilla/gradle_jarsigner/JarSignerVerifyPlugin.groovy
@@ -10,41 +10,76 @@ import java.nio.file.Paths
 
 class JarSignerVerifyPluginExtension {
     List dependencies
+    Map certificates
 }
 
 class JarSignerVerifyPlugin implements Plugin<Project> {
 
-    static String DEFAULT_ALIAS = 'non-important-alias'
+    void apply(Project project) {
+        project.extensions.create("signatureVerification", JarSignerVerifyPluginExtension)
+        project.afterEvaluate {
+            def tempDir = File.createTempDir("gradle-jarsigner", "tmp")
+            String tempDirPath = tempDir.toString()
 
-    static populateKeystore(certificatePath, tempDirPath) {
-        String command = craftKeystoreCreationCommand(certificatePath, tempDirPath)
+            try {
+                populateKeystore(tempDirPath, project.signatureVerification.certificates)
+                verifyDependencies(project, tempDirPath, project.signatureVerification.dependencies)
+            } finally {
+                tempDir.deleteDir()
+            }
+        }
+    }
+
+    static populateKeystore(keystoreFolder, certificatePathPerAlias) {
         def password = Utils.generateRandomPassword(32)
-        Utils.shellOut(command, [password, password])
+        certificatePathPerAlias.each { alias, certificatePath ->
+            String command = craftKeystoreCreationCommand(keystoreFolder, certificatePath, alias)
+            def processData = Utils.shellOut(command, [password, password])
+            if (processData.exitCode != 0) {
+                throw new InvalidUserDataException("Cannot insert certificate ${certificatePath}. out> $stdOut err> $stdErr")
+            }
+        }
+    }
+
+    static craftKeystoreCreationCommand(keystoreFolder, certificatePath, certificateAlias) {
+        String keystorePath = getKeyStorePath(keystoreFolder)
+        return """keytool -importcert -v -noprompt \
+-file ${certificatePath} \
+-alias ${certificateAlias} \
+-keystore ${keystorePath}"""
     }
 
     static getKeyStorePath(keystoreFolder) {
         return Paths.get(keystoreFolder, "keystore")
     }
 
-    static craftKeystoreCreationCommand(certificatePath, keystoreFolder) {
-        String keystorePath = getKeyStorePath(keystoreFolder)
-        return """keytool -importcert -v -noprompt \
--file ${certificatePath} \
--alias ${DEFAULT_ALIAS} \
--keystore ${keystorePath}"""
+    static verifyDependencies(project, tempDirPath, dependencies) {
+        dependencies.each { assertion ->
+            verifySingleDependency(project, tempDirPath, assertion)
+        }
     }
 
-    static craftJarsignerVerifyCommand(keystoreFolder, dependencyPath) {
-        String keystorePath = getKeyStorePath(keystoreFolder)
-        // -strict enforces the alias to match
-        return """jarsigner -verify -strict -verbose \
--keystore ${keystorePath} \
-${dependencyPath} \
-${DEFAULT_ALIAS}"""
+    static verifySingleDependency(project, keystoreFolder, assertion) {
+        List  parts  = assertion.tokenize(":")
+        String group = parts.get(0)
+        String name  = parts.get(1)
+        String certificateAlias = parts.get(2)
+
+        ResolvedArtifact dependency = project.configurations.compile.resolvedConfiguration.resolvedArtifacts.find {
+            return it.name.equals(name) && it.moduleVersion.id.group.equals(group)
+        }
+
+        println "Verifying the signature of ${group}:${name} against certificate alias '${certificateAlias}'"
+
+        if (dependency == null) {
+            throw new InvalidUserDataException("No dependency for integrity assertion found: ${group}:${name}")
+        }
+
+        verifyJar(keystoreFolder, dependency.file, certificateAlias)
     }
 
-    static verifyJar(keystoreFolder, jarFile) {
-        def command = craftJarsignerVerifyCommand(keystoreFolder, jarFile)
+    static verifyJar(keystoreFolder, jarFile, certificateAlias) {
+        def command = craftJarsignerVerifyCommand(keystoreFolder, jarFile, certificateAlias)
         def processData = Utils.shellOut(command)
 
         processData.with {
@@ -57,35 +92,12 @@ ${DEFAULT_ALIAS}"""
         }
     }
 
-    void apply(Project project) {
-        project.extensions.create("signatureVerification", JarSignerVerifyPluginExtension)
-        project.afterEvaluate {
-            project.signatureVerification.dependencies.each {
-                assertion ->
-                    List  parts  = assertion.tokenize(":")
-                    String group = parts.get(0)
-                    String name  = parts.get(1)
-                    String certificatePath = parts.get(2)
-
-                    ResolvedArtifact dependency = project.configurations.compile.resolvedConfiguration.resolvedArtifacts.find {
-                        return it.name.equals(name) && it.moduleVersion.id.group.equals(group)
-                    }
-
-                    println "Verifying the signature of ${group}:${name} against certificate '${certificatePath}'"
-
-                    if (dependency == null) {
-                        throw new InvalidUserDataException("No dependency for integrity assertion found: ${group}:${name}")
-                    }
-
-                    def tempDir = File.createTempDir("gradle-jarsigner", "tmp")
-                    try {
-                        String tempDirPath = tempDir.toString()
-                        populateKeystore(certificatePath, tempDirPath)
-                        verifyJar(tempDirPath, dependency.file)
-                    } finally {
-                        tempDir.deleteDir()
-                    }
-            }
-        }
+    static craftJarsignerVerifyCommand(keystoreFolder, dependencyPath, certificateAlias) {
+        String keystorePath = getKeyStorePath(keystoreFolder)
+        // -strict enforces the alias to match
+        return """jarsigner -verify -strict -verbose \
+-keystore ${keystorePath} \
+${dependencyPath} \
+${certificateAlias}"""
     }
 }
